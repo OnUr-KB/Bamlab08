f40dd1c6a08f01904705d73f6acdc87949e22702
OnUr-KB <kkanya0947@gmail.com>
2023-12-12 16:34:26 +0700

    finish

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,65 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float age, height, bounty;
+    cout << "Enter your age: \n";
+    cin >> age;
+
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if (height < 100)
+        {
+            cout << "Your character = Chopper";
+            return 0;
+        }
+        if (height < 180)
+        {
+            cout << "Your character = Usopp";
+            return 0;
+        }
+        if (height > 180)
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+        }
+        if (bounty < 11000000000)
+        {
+            cout << "Your character = Sanji";
+            return 0;
+        }
+        if (bounty > 11000000000)
+        {
+            cout << "Your character = Zoro";
+            return 0;
+        }
+    }
+    if (age > 60)
+    {
+        cout << "Your character = Brook";
+        return 0;
+    }
+
+    if (age < 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+    }
+    if (bounty < 5000000000)
+    {
+        cout << "Your character = Franky";
+        return 0;
+    }
+    if (bounty > 5000000000)
+    {
+        cout << "Your character = Jinbe";
+        return 0;
+    }
+}
